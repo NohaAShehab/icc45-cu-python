51ec49aa5726dc1038bdef7cb9d8fe22c5ef308c
Noha Shehab <nohashehab.iti@gmail.com>
2025-01-04 12:26:03 +0200

    oop principles

--- playground.py
+++ playground.py
@@ -1,3 +1,13 @@
 
-class Employee:
-    pass
+# class Employee:
+#     pass
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def speak(self):
+        print(f'My name is {self.name} and I am {self.age} years old.')
+
+p = Person('John', 36)
+print(isinstance(p, object))
